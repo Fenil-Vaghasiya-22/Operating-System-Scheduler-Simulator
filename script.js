eecd83f7536ec6ad4c81c57be40d741010b2dc21
Fenil-Vaghasiya-22 <166972129+Fenil-Vaghasiya-22@users.noreply.github.com>
2025-04-04 09:50:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,24 +20,66 @@ function displayInputTable(){
     document.getElementById('save').innerHTML=saveData;
 }
 
-function saveData(){
+// function saveData(){
+//     let size = document.getElementById('nprocesses').value;
+//     let processes = []; 
+
+//     for (let i = 0; i < size; i++) {
+//         let process = {};
+//         process.processId = "P" + (i + 1);
+//         process.arrivalTime = parseInt(document.getElementById('arrival_'+ (i+1)).value);
+//         process.burstTime = parseInt(document.getElementById('burst_' + (i+1)).value);
+//         process.priority = parseInt(document.getElementById('priority_' + (i+1)).value);
+//         processes.push(process);
+//     }
+//     var options = "<p>Select One of the following Algorithm</p><br>"
+//     options += "<button onclick='FCFS("+JSON.stringify(processes)+")'>FCFS</button>" + " " +
+//     "<button onclick='RR("+JSON.stringify(processes)+")'>Round Robin</button>" + " " +
+//     "<button onclick='SJF("+JSON.stringify(processes)+")'>SJF</button>" + " " +
+//     "<button onclick='SRTN("+JSON.stringify(processes)+")'>SRTN</button>" + " " +
+//     "<button onclick='Priority("+JSON.stringify(processes)+")'>Priority</button>";
+//     document.getElementById('options').innerHTML=options;
+// }
+
+function saveData() {
     let size = document.getElementById('nprocesses').value;
-    let processes = []; 
+    let processes = [];
 
     for (let i = 0; i < size; i++) {
-        let process = {};
-        process.processId = "P" + (i + 1);
-        process.arrivalTime = parseInt(document.getElementById('arrival_'+ (i+1)).value);
-        process.burstTime = parseInt(document.getElementById('burst_' + (i+1)).value);
-        process.priority = parseInt(document.getElementById('priority_' + (i+1)).value);
+        let process = {
+            processId: "P" + (i + 1),
+            arrivalTime: parseInt(document.getElementById('arrival_' + (i + 1)).value),
+            burstTime: parseInt(document.getElementById('burst_' + (i + 1)).value),
+            priority: parseInt(document.getElementById('priority_' + (i + 1)).value)
+        };
         processes.push(process);
     }
-    var options = "<p>Select One of the following Algorithm</p><br>"
-    options += "<button onclick='FCFS("+JSON.stringify(processes)+")'>FCFS</button>" + " " +
-    "<button onclick='RR("+JSON.stringify(processes)+")'>Round Robin</button>" + " " +
-    "<button onclick='SJF("+JSON.stringify(processes)+")'>SJF</button>" + " " +
-    "<button onclick='SRTN("+JSON.stringify(processes)+")'>SRTN</button>" + " " +
-    "<button onclick='Priority("+JSON.stringify(processes)+")'>Priority</button>";
-    document.getElementById('options').innerHTML=options;
-}
 
+    // Save to MongoDB via backend
+    fetch("https://backend-hzzu.onrender.com/api/save", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(processes)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        alert("Data saved to MongoDB!");
+    })
+    .catch(err => {
+        console.error(err);
+        alert("Error saving data.");
+    });
+
+    // Same options code as before
+    let options = "<p>Select One of the following Algorithm</p><br>" +
+    "<button onclick='FCFS(" + JSON.stringify(processes) + ")'>FCFS</button> " +
+    "<button onclick='RR(" + JSON.stringify(processes) + ")'>Round Robin</button> " +
+    "<button onclick='SJF(" + JSON.stringify(processes) + ")'>SJF</button> " +
+    "<button onclick='SRTN(" + JSON.stringify(processes) + ")'>SRTN</button> " +
+    "<button onclick='Priority(" + JSON.stringify(processes) + ")'>Priority</button>";
+
+    document.getElementById('options').innerHTML = options;
+}
